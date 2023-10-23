2c68c620eb04b87d4b6d1567c65745a7a92d6088
dmfv <56220925+dmfv@users.noreply.github.com>
2023-10-23 19:30:55 +0200

    Create HappyBrithday.sv

---
+++ HappyBrithday.sv
@@ -0,0 +1,7 @@
+module main;
+  initial begin
+    $display("С днем рождения. Желаю тебе  успехов. счастья, здоровья, успеха по жизни и немного в работе"); // non_trivial_congratulations,
+    $display("И пусть следующий тортик будет от Intel, вместе с предложением о сотрудничестве"); // dream_company
+    $display("Уверен, у тебя все получится!");
+  end
+endmodule
