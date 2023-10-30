b33686c27aa2b0aa20081b58bbbc77daa9398242
dmfv <56220925+dmfv@users.noreply.github.com>
2023-10-30 11:49:33 +0100

    comma fix

--- HappyBrithday.sv
+++ HappyBrithday.sv
@@ -1,6 +1,6 @@
 module main;
   initial begin
-    $display("С днем рождения. Желаю тебе  успехов. счастья, здоровья, успеха по жизни и немного в работе"); // non_trivial_congratulations,
+    $display("С днем рождения. Желаю тебе  счастья, здоровья, успеха по жизни и в работе"); // non_trivial_congratulations
     $display("И пусть следующий тортик будет от Intel, вместе с предложением о сотрудничестве"); // dream_company
     $display("Уверен, у тебя все получится!");
   end
